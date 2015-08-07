b6a175920c5c5ada8f33c190a5d063702a651694
Emma Peng <u3502350@connect.hku.hk>
2015-08-07 18:17:14 +0800

    Create InvertBinaryTree.py

---
+++ easy_py/InvertBinaryTree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {TreeNode}
+    def invertTree(self, root):
+        if root == None: return root
+        
+        left = root.left
+        right = root.right
+        
+        root.left = self.invertTree(right)
+        root.right = self.invertTree(left)
+        
+        return root
+        
